d4184d7f2e1bb84c81323cef90720ebe57518205
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-08-07 21:29:53 +0300

    creating the main classes

---
+++ challenges/ch-10-stack/stack-queue/stack_queue/stack_queue.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,value):
+        self.value=value
+        self.next=None
+
+
+class Stack:
+    def __init__(self):
+        self.top = None
+
+
+class Queue():
+    def __init__(self):
+        self.front = None
+        self.rear = None
