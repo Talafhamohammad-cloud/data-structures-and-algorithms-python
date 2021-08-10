3381b0c389a8ef25cd5e1f8715a7efda3309e423
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-08-10 17:09:55 +0300

    finish all req of ch-12

--- challenges/ch-12/animal-shelter/animal_shelter/animal_shelter.py
+++ challenges/ch-12/animal-shelter/animal_shelter/animal_shelter.py
@@ -1,16 +1,74 @@
-class Animal:
-    def __init__(self, name, type):
-        self.name = name
-        self.type = type
+class Node:
+  def __init__(self, value):
+    self.value = value
+    self.next = None
 
 
-class Dog:
-    def __init__(self, name):
-        self.name = name
-        self.type = 'dog'
+class Queue():
+    def __init__(self):
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, value):
+        node = Node(value)
+        if self.front == None:
+            self.front = node
+            self.rear = node
+            return self.rear.value
+        else:
+            self.rear.next = node
+            self.rear = node
+            return self.rear.value
+
+    def dequeue(self):
+        try:
+            if self.front == None:
+              raise Exception
+            temp = self.front
+            self.front = self.front.next
+            return temp.value
+        except Exception:
+            return 'error'
+
+    def isEmpty(self):
+        return self.front == None
+
+    def peek(self):
+        try:
+            if self.front == None:
+                raise Exception
+            return self.front.value
+        except Exception:
+            return 'Empty Queue'
 
 
-class Cat:
-    def __init__(self, name):
-        self.name = name
-        self.type = 'cat'
+class AnimalShelter:
+    def __init__(self):
+        self.cat = Queue()
+        self.dog = Queue()
+        self.count = 0
+
+    def enqueue(self, animal, type):
+        if type == 'cat':
+            self.count += 1
+            return self.cat.enqueue(animal)
+        elif type == 'dog':
+            self.count += 1
+            return self.dog.enqueue(animal)
+        else:
+            return 'you have to choose cat or dog'
+
+    def dequeue(self, pref):
+        if pref == 'dog':
+            return self.dog.dequeue()
+        if pref == 'cat':
+            return self.cat.dequeue()
+        else:
+            return 'you have to choose cat or dog'
+
+    def is_empty(self):
+        if self.count > 0:
+            return False
+        else:
+            return True
+
