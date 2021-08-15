50b4fa0ee58f76726102009412ca903ebd5b3572
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-08-15 06:45:40 +0300

    finish ch-15

--- challenges/ch-15-trees/tree-ch/tests/test_tree_ch.py
+++ challenges/ch-15-trees/tree-ch/tests/test_tree_ch.py
@@ -1,5 +1,99 @@
 from tree_ch import __version__
-
+from tree_ch.tree_ch import binarysearchtree
 
 def test_version():
     assert __version__ == '0.1.0'
+######################################################################################
+# first test "successfully instantiate an empty tree"
+def test_empty_tree():
+    tree = binarysearchtree()
+    actual = tree.root
+    expected = None
+    assert actual == expected
+#######################################################################################
+# second test "successfully instantiate a tree with a single root node"
+def test_single_root_node():
+    tree = binarysearchtree()
+    tree.add(1)
+    tree.add(2)
+    tree.add(3)
+    tree.add(4)
+    actual = tree.root.value
+    expected = 1
+    assert actual == expected
+########################################################################################
+# third test "successfully add a left child and right child to a single root node"
+def test_left_and_right_single_root_node():
+    tree = binarysearchtree()
+    tree.add(10)
+    tree.add(2)
+    tree.add(30)
+    actual = (tree.root.left.value, tree.root.right.value)
+    expected = (2,30)
+    assert actual == expected
+###########################################################################################
+# fourth test "successfully return a collection from an inorder traversal"
+def test_inorder_traversal():
+    tree = binarysearchtree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(2)
+    tree.add(7)
+    tree.add(11)
+    tree.add(3)
+    actual = tree.inorder()
+    expected = [1, 2, 3, 5, 7, 11]
+    assert actual == expected
+###########################################################################################
+# fifth test "successfully return a collection from a preorder traversal"
+def test_preorder_traversal():
+    tree = binarysearchtree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(2)
+    tree.add(7)
+    tree.add(11)
+    tree.add(3)
+    actual = tree.preorder()
+    expected = [1, 5, 2, 3, 7, 11]
+    assert actual == expected
+##########################################################################################
+# sixth test "successfully return a collection from a postorder traversal"
+def test_postorder_traversal():
+    tree = binarysearchtree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(2)
+    tree.add(7)
+    tree.add(11)
+    tree.add(3)
+    actual = tree.postorder()
+    expected = [3, 2, 11, 7, 5, 1]
+    assert actual == expected
+############################################################################################
+# extra test "successfully return the value "dose not exist" in the tree "
+def test_value_dne():
+    tree = binarysearchtree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(2)
+    tree.add(7)
+    tree.add(11)
+    tree.add(3)
+    actual = tree.Contains(4)
+    expected = "dose not exist"
+    assert actual == expected
+################################################################################################
+# extra test "successfully return if the valeu exist in the tree "
+def test_value_exist():
+    tree = binarysearchtree()
+    tree.add(1)
+    tree.add(5)
+    tree.add(2)
+    tree.add(7)
+    tree.add(11)
+    tree.add(3)
+    actual = tree.Contains(11)
+    expected = "the value is exist"
+    assert actual == expected
+#################################################################################################
--- challenges/ch-15-trees/tree-ch/tree_ch/tree_ch.py
+++ challenges/ch-15-trees/tree-ch/tree_ch/tree_ch.py
@@ -1,10 +1,101 @@
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, val):
+        self.value = val
         self.left = None
         self.right = None
-
-
-class BinaryTree:
+###########################################################################################
+class binarytree:
     def __init__(self):
         self.root = None
+    def inorder(self):
+            self.values = []
+            if not self.root:
+                return "empty tree"
+            def ordering(node):
+                if node.left:
+                    ordering(node.left)
+                self.values += [node.value]
+                if node.right:
+                    ordering(node.right)
+                return self.values
+            return ordering(self.root)
+##################################################################################################
+    def preorder(self):
+            self.values = []
+            if self.root == None:
+                return "empty tree"
+            def ordering(node):
+                self.values += [node.value]
+                if node.left:
+                    ordering(node.left)
+                if node.right:
+                    ordering(node.right)
+                return self.values
+            return ordering(self.root)
+###################################################################################################
+    def postorder(self):
+            self.values = []
+            if not self.root:
+                return "empty tree"
+            def ordering(node):
+                if node.left:
+                    ordering(node.left)
+                if node.right:
+                    ordering(node.right)
+                self.values += [node.value]
+                return self.values
+            return ordering(self.root)
+##################################################################################################
+class binarysearchtree(binarytree):
+    def add(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        else:
+            current = self.root
+            while current:
+                if value < current.value:
+                    if current.left == None:
+                        current.left = Node(value)
+                        break
+                    current = current.left
+                else:
+                    if current.right == None:
+                        current.right = Node(value)
+                        break
+                    current = current.right
+########################################################################################################
+    def Contains(self, value):
+        if self.root == None:
+            return 'empty tree'
+        else:
+            current = self.root
+            while current:
+                if current.value == value:
+                    return "the value is exist"
+                elif value < current.value:
+                    if current.left == None:
+                        return "dose not exist"
+                    current = current.left
+                else:
+                    if current.right == None:
+                        return "dose not exist"
+                    current = current.right
+
+
+####################################################################
+# Driver code "for quick test"
+#if __name__ == '__main__':
+#    tree = binarysearchtree()
+#    tree.add(1)
+#    tree.add(5)
+#    tree.add(2)
+#    tree.add(7)
+#    tree.add(11)
+#    tree.add(3)
+#    tree2 =binarysearchtree()
+#print(tree.inorder())
+#print(tree.postorder())
+#print(tree.preorder())
+#print(tree.Contains(6))
+#print(tree2.Contains(6))
+####################################################################################################
