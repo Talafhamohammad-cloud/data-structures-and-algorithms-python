2f70408f542ebd790c4ceae388c53138ffbe2672
Talafhamohammad-cloud <josephtelfah@icloud.com>
2021-08-29 05:48:25 +0300

    finish ch-18

--- challenges/ch-18/tree-fizz-buzz/tests/test_tree_fizz_buzz.py
+++ challenges/ch-18/tree-fizz-buzz/tests/test_tree_fizz_buzz.py
@@ -1,5 +1,11 @@
 from tree_fizz_buzz import __version__
-
+from tree_fizz_buzz.tree_fizz_buzz import *
 
 def test_version():
     assert __version__ == '0.1.0'
+############################################################################# Test if its empty
+def test_empty_tree():
+    tree = ktree()
+    assert fizz_buzz(tree) == "empty tree"
+
+
--- challenges/ch-18/tree-fizz-buzz/tree_fizz_buzz/tree_fizz_buzz.py
+++ challenges/ch-18/tree-fizz-buzz/tree_fizz_buzz/tree_fizz_buzz.py
@@ -1,10 +1,37 @@
 class Node:
-    def __init__(self, value,left, right):
+    def __init__(self, value):
         self.value = value
-        self.left = left
-        self.right = right
-
-class K_aryTree:
-
-    def __init__(self, root):
-        self.root = root
+        self.left = None
+        self.right = None
+class ktree:
+    def __init__(self):
+        self.root = None
+    def preorder(self):
+        self.values = []
+        if self.root == None:
+            return "empty tree"
+        def ordering(node):
+            self.values += [node.value]
+            if node.left:
+                ordering(node.left)
+            if node.right:
+                ordering(node.right)
+            return self.values
+        return ordering(self.root)
+def fizz_buzz(ktree):
+    if not ktree.root:
+        return "empty tree"
+    transformation = []
+    def ordering(root):
+        if root.value == None:
+           return
+        if root.value % 3 == 0 and root.value % 5 == 0:
+            root.value = "fizzbuzz"
+        elif root.value % 3 == 0:
+            root.value == "fizz"
+        elif root.value % 5 == 0:
+            root.value = "buzz"
+        transformation.append(root.value)
+        ordering(root.left)
+        ordering(root.right)
+        return transformation
